c579a0c57ac54cf98f53f815cb012e446cb57dab
creyes <creyes@e-contact.cl>
2021-12-29 02:45:58 -0300

    datatable implementation

--- client/src/components/clientes/list.js
+++ client/src/components/clientes/list.js
@@ -1,172 +1,191 @@
-import React from 'react';
-import { Table } from 'antd';
-import { FaRegEye } from "react-icons/fa";
-import { RiDeleteBin6Fill } from "react-icons/ri";
-import { AiFillEdit } from "react-icons/ai";
-import { Link } from "react-router-dom";
-import Swal from "sweetalert2";
-import axios from 'axios';
-import '../../App.css';
+import { forwardRef, useRef, useState } from 'react'
+//import MaterialTable from "material-table";
+import MaterialTable from 'material-table';
+import { Col } from 'reactstrap';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
 
-const ClienteList = ({clientes, eliminar: eliminarClientes}) => {
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 
-    const eliminar = (e, id) => {
-        e.stopPropagation();
-        if(id) {
-            eliminarClientes(id);
-            Swal.fire({
-                title:'Eliminar el Cliente Definitivamente',
-                text: '¿Esta seguro que desea eliminar el Cliente',
-                icon:'question',
-                showCancelButton: true,
-                confirmButtonText: 'Si, eliminar!!!',
-                cancelButtonText: 'No'
-            }).then(result => {
-                if(result.value) {
-                    axios.drop(`/api/clientes/${id}`)
-                    .then(resp => {
-                    Swal.fire('Cliente Eliminado', 'El Cliente ha sido eliminado', 'success')
-                    })
-                    .catch(error => Swal.fire('Error al eliminar el Cliente', error?.message, 'error'));
-                }
-            })
-        }
-    }
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddCircleOutlineIcon color={'primary'} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteForeverIcon color={'error'} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <EditIcon color={'success'} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterAltIcon color="action" ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
-    const columns = [
-        {
-            title: 'Nombre',
-            dataIndex: 'firstName',
-            key: 'firstName',
-            firstName: '',
-            sorter: (a, b) => a.firstName.localeCompare(b.firstName),
-            sortDirections: ['descend', 'ascend'],
-            ellipsis: true,
-            width: '15%',
-        },
-        {
-            title: 'Apellido',
-            dataIndex: 'lastName',
-            key: 'lastName',
-            lastName: '',
-            sorter: (a, b) => a.lastName.localeCompare(b.lastName),
-            sortDirections: ['descend', 'ascend'],
-            ellipsis: true,
-            width: '20%',
-        },
-        {
-        key: 'rut',
-        rut: '',
-        title: 'RUT',
-        dataIndex: 'rut',
-        sorter: (a, b) => a.rut.localeCompare(b.rut),
-        width: '17%',
-        },
-        {
-        key: 'email',
-        email: '',
-        title: 'Email',
-        dataIndex: 'email',
-        sorter: (a, b) => a.email.localeCompare(b.email),
-        width: '15%',
-        },
-        {
-        key: 'phone',
-        phone: '',
-        title: 'Teléfono',
-        dataIndex: 'phone',
-        sorter: (a, b) => a.phone.localeCompare(b.phone),
-        width: '15%',
-        },
-        {
-        key: 'estufas',
-        estufa: '',
-        title: 'Estufas',
-        dataIndex: 'estufa',
-        sorter: (a, b) => a.estufa.localeCompare(b.estufa),
-        width: '20%',
-        },
-        {
-        title: 'Acciones',
-        dataIndex: 'actions',
-        render: (text, record) => (
-            <span>
-                <Link to={`/clientes/view/${record.id}`}><FaRegEye color="blue" style={{ marginRight: '10px'}}/></Link>
-                <Link to={`/clientes/update/${record.id}`}><AiFillEdit color="green" style={{ marginRight: '10px'}} /></Link>
-                <RiDeleteBin6Fill color="red" onClick={e => eliminar(e, record.id)} style={{cursor: 'pointer'}}></RiDeleteBin6Fill>
-            </span>
-            ),
-        },
-    ];
+const ClienteList = () => {
+    const nodeRef = useRef(null);
+    //const [data, setData] = useState(originalData);
 
-    const data = clientes?.map(cliente => ({
-        key: cliente._id,
-        id: cliente._id,
-        firstName: cliente.firstName,
-        lastName: cliente.lastName,
-        email: cliente.email,
-        rut: cliente.rut,
-        phone: cliente.phone,
-        estufa: cliente.estufa.nombreEstufa + ' / ' +  cliente.estufa.marcaEstufa,
-        mantenciones: cliente.mantencion,
-    }));
+    const [columns, setColumns] = useState([
+        { field: "id", hidden: true },
+        { title: 'Name', field: 'name' },
+        { title: 'Surname', field: 'surname', initialEditValue: 'initial edit value' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+            title: 'Birth Place',
+            field: 'birthCity',
+            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+    ]);
 
-    const config = { 
-        pagination : {
-            defaultPageSize: 10,
-            pageSizeOptions : ['10', '20', '30'], 
-            showSizeChanger : true,
-            showQuickJumper : false,
-            showTotal : total => `En total son ${total} registros`
-        }
-    }
+    const [data, setData] = useState([
+        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+    ]);
 
     return (
-        <>
-            <Table columns={columns} dataSource={data} {...config}/>
-        </>
+        <Col>
+            <MaterialTable
+                nodeRef={nodeRef}
+                title=""
+                columns={columns}
+                data={data}
+                icons={tableIcons}
+                // actions={[
+                //     {
+                //         icon: () => <AddCircleOutlineIcon color={'primary'} />,
+                //         tooltip: 'Agregar Cliente',
+                //         isFreeAction: true,
+                //         onClick: (event) => alert("You want to add a new row")
+                //     },
+                //     {
+                //         icon: () => <EditIcon color={'success'} />,
+                //         tooltip: 'Editar Cliente',
+                //         onClick: (event, rowData) => alert("You saved " + JSON.stringify(rowData))
+                //     },
+                //     {
+                //         icon: () => <DeleteForeverIcon color={'error'} />,
+                //         tooltip: 'Eliminar Cliente',
+                //         onClick: (event, rowData) => alert("You want to delete " + JSON.stringify(rowData))
+                //     }
+                // ]}
+                localization={{
+                    toolbar: {
+                        exportCSVName: "Exportar como CSV",
+                        exportPDFName: "Exportar como PDF",
+                        exportTitle: "Exportar",
+                        searchTooltip: "Buscar",
+                        searchPlaceholder: "Buscar"
+                    },
+                    header:{
+                        actions: "Acciones"
+                    },
+                    body: {
+                        addTooltip: "Crear",
+                        editTooltip: "Editar",
+                        deleteTooltip: "Eliminar",
+                        editRow: {
+                            deleteText: "Está seguro que desea eliminar el cliente",
+                            cancelTooltip: "Cancelar",
+                            saveTooltip: "Aceptar"
+                        }
+                    },
+                    pagination: {
+                        labelRowsSelect: "filas",
+                        labelDisplayedRows: "{from}-{to} de {count}",
+                        labelRowsPerPage: "Filas por página:",
+                        firstAriaLabel: "Primera página",
+                        firstTooltip: "Primera página",
+                        previousAriaLabel: "Página anterior",
+                        previousTooltip: "Página anterior",
+                        nextAriaLabel: "Siguiente página",
+                        nextTooltip: "Siguiente página",
+                        lastAriaLabel: "Última página",
+                        lastTooltip: "Última página"
+                    },
+                    grouping: {
+                        placeholder: "Arrastra los encabezados aquí para agruparlos",
+                        groupedBy: "Agrupados por: "
+                    }
+                }}
+                options={{
+                    actionsColumnIndex: -1,
+                    searchFieldAlignment: 'left',
+                    exportButton: {
+                        csv: true,
+                        pdf: true
+                    },
+                    grouping: true,
+                    filtering: true
+                }}
+                editable={{
+                    onRowAdd: newData =>
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                setData([...data, newData]);
+                                resolve();
+                            }, 1000)
+                        }),
+                    onRowUpdate: (newData, oldData) =>
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                const dataUpdate = [...data];
+                                const index = oldData.tableData.id;
+                                dataUpdate[index] = newData;
+                                setData([...dataUpdate]);
+                                resolve();
+                            }, 1000)
+                        }),
+                    onRowDelete: oldData =>
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                const dataDelete = [...data];
+                                const index = oldData.tableData.id;
+                                dataDelete.splice(index, 1);
+                                setData([...dataDelete]);
+                                resolve()
+                            }, 1000)
+                        }),
+                }}
+                detailPanel={(rowData) => {
+                    console.log(rowData);
+                    return (
+                        <div
+                            style={{
+                                fontSize: 20,
+                                textAlign: 'left',
+                                color: 'white',
+                                backgroundColor: '#43A047',
+                            }}
+                        >
+                            {JSON.stringify(rowData)}
+                        </div>
+                    )
+                }}
+            />
+        </Col>
     )
 }
+
 export default ClienteList;
-
-    {/*return (
-    <Container fluid className="espaciado2">
-        <Row>
-            <Col xs={12}>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Nombre:</th>
-                            <th>Apellido:</th>
-                            <th>RUT:</th>
-                            <th>Email:</th>
-                            <th>Teléfono:</th>
-                            <th>Tipo de Estufa:</th>
-                            <th>Fecha Mantención:</th>
-                            <th>Acciones:</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.clientes && props.clientes.map((cliente, i) => <tr key={i}>
-                            <td>{cliente.firstName}</td>
-                            <td>{cliente.lastName}</td>
-                            <td>{cliente.rut}</td>
-                            <td>{cliente.email}</td>
-                            <td>{cliente.phone}</td>
-                            <td>{cliente.estufa.nombreEstufa} / {cliente.estufa.marcaEstufa}</td>
-                            <td>{cliente.mantencion}</td>
-                            <td>
-                                <Link to={`/clientes/view/${cliente._id}`}><FaRegEye color="blue" style={{ marginRight: '10px'}}/></Link>
-                               
-                                <Link to={`/clientes/update/${cliente._id}`}><AiFillEdit color="green" style={{ marginRight: '10px'}} /></Link>
-                                <RiDeleteBin6Fill color="red" onClick={e => eliminar(e, cliente._id)} style={{cursor: 'pointer'}}></RiDeleteBin6Fill>
-                            </td>
-                        </tr>)}
-                    </tbody>
-                </Table>
-            </Col>
-        </Row>
-    </Container>
-    );
-}*/}
-
